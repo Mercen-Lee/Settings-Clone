0753584cae7aca8492a5c72ab506f4b63dd081dc
이석호 <mercen@mercen.net>
2022-07-28 11:40:25 +0900

    Initial Commit

---
+++ Settings/SettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsApp.swift
+//  Settings
+//
+//  Created by Mercen on 2022/07/28.
+//
+
+import SwiftUI
+
+@main
+struct SettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
